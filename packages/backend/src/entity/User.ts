712a4910700f47eb1c60b1c625f350b7c5e6e76f
Matheus Antonio <matheusantoniof@gmail.com>
2020-09-14 22:52:53 -0300

    feat: config typeorm

---
+++ packages/backend/src/entity/User.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
+
+export default User;
